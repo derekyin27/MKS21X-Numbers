43134f3998e48f167846a6a3b1bc44246ce11225
derekyin27 <43381977+derekyin27@users.noreply.github.com>
2018-10-15 21:44:14 -0400

    Update RationalNumber.java

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,1 +1,21 @@
-
+public class RationalNumber{
+  private int n;
+  private int d;
+  public RationalNumber(int num, int denom) {
+    n = num;
+    d = denom;
+  }
+  
+  public int getN() {
+    return n;
+  }
+  
+  public int getD() {
+    return d;
+  }
+  
+  public double getValue() {
+    return getN() / getD();
+  }
+  
+}
