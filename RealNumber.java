cfd80838c2697c83b5d9c37e94f0b4a67e2fc48d
derekyin27 <43381977+derekyin27@users.noreply.github.com>
2018-10-15 21:53:20 -0400

    Update RealNumber.java

--- RealNumber.java
+++ RealNumber.java
@@ -1,9 +1,15 @@
 public class RealNumber{
   private double value;
   
+  public RealNumber(double VAL){
+    return value;
+  }
+  
   public double getValue(){
     return value;
   }
+  public String toString(){
+    return "a";
 }
   
   
